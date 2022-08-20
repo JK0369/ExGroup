c19b93421e653825c674dc0fae8d2422940d35a0
jake <palatable7@naver.com>
2022-08-20 16:12:26 +0900

    Initial Commit

---
+++ ExGroup/ExGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExGroupApp.swift
+//  ExGroup
+//
+//  Created by 김종권 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct ExGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
