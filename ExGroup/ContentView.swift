35b560edd9c7e385890e29c9ffbecc145163486c
jake <palatable7@naver.com>
2022-08-21 12:08:40 +0900

    Implements

--- ExGroup/ContentView.swift
+++ ExGroup/ContentView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+enum NotifyMeAboutType {
+  case directMessages
+  case mentions
+  case anything
+}
+
+enum Flavor: String, CaseIterable, Identifiable {
+  case chocolate, vanilla, strawberry
+  var id: Self { self }
+}
+
 struct ContentView: View {
   @State var userAgreed = false
   let text = """
@@ -15,22 +26,66 @@ Use a group box when you want to visually distinguish a portion of your user int
 The following example sets up a GroupBox with the label “End-User Agreement”, and a long agreementText string in a Text view wrapped by a ScrollView. The box also contains a Toggle for the user to interact with after reading the text.
 """
   
+  @State var notifyMeAbout = NotifyMeAboutType.directMessages
+  @State var playNotificationSounds = false
+  @State var sendReadReceipts = false
+  
+  @State private var selectedFlavor = Flavor.chocolate
+  
   var body: some View {
     VStack {
-      GroupBox {
-        Text("Content")
-      }
-      GroupBox("title") {
-        Text("Content")
-      }
-      GroupBox(label: Label("End-User Agreement", systemImage: "building.columns")) {
-        ScrollView(.vertical, showsIndicators: true) {
-          Text(text)
-            .font(.footnote)
+//      GroupBox {
+//        Text("Content")
+//      }
+//      GroupBox("title") {
+//        Text("Content")
+//      }
+//      GroupBox(label: Label("End-User Agreement", systemImage: "building.columns")) {
+//        ScrollView(.vertical, showsIndicators: true) {
+//          Text(text)
+//            .font(.footnote)
+//        }
+//        .frame(height: 100)
+//        Toggle(isOn: $userAgreed) {
+//          Text("I agree to the above terms")
+//        }
+//      }
+      
+      // Ex_Section_Form
+//      Form {
+//        Section(header: Text("Form_Section1")) {
+//          Picker("Notify Me About", selection: $notifyMeAbout) {
+//            Text("Direct Messages").tag(NotifyMeAboutType.directMessages)
+//            Text("Mentions").tag(NotifyMeAboutType.mentions)
+//            Text("Anything").tag(NotifyMeAboutType.anything)
+//          }
+//          Toggle("Play notification sounds", isOn: $playNotificationSounds)
+//          Toggle("Send read receipts", isOn: $sendReadReceipts)
+//        }
+//
+//        Section(header: Text("Form_Section2")) {
+//          Text("Section2 Contents")
+//        }
+//      }
+      
+      // Ex_Section_List
+//      List {
+//        Section("List_Section1") {
+//          Text("Content1")
+//        }
+//        Section("List_Section2") {
+//          Text("Content2")
+//        }
+//      }
+      
+      // Ex_Section_Picker
+      Picker("Flavor", selection: $selectedFlavor) {
+        Section(header: Text("Picker_Section1")) {
+          Text("Chocolate").tag(Flavor.chocolate)
         }
-        .frame(height: 100)
-        Toggle(isOn: $userAgreed) {
-          Text("I agree to the above terms")
+        Section(header: Text("Picker_Section2")) {
+          Text("Vanilla").tag(Flavor.vanilla)
+          Text("Strawberry").tag(Flavor.strawberry)
         }
       }
     }
